3032274207187d0c0e4ae777b957ad125118f517
audishos <audisho.sada@gmail.com>
2017-09-07 11:38:27 -0400

    Adds completed advancedSorting.js exercise

---
+++ w1d4/advancedSorting.js
@@ -0,0 +1,27 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a, b){
+  //sort by name asceding
+  if (a.name > b.name) {
+    return 1;
+  }
+  else if (a.name < b.name) {
+    return -1;
+  }
+  else {
+    //if names are equal, sort by age descending
+    if (a.age > b.age) {
+      return -1;
+    }
+    else {
+      return 1;
+    }
+  }
+});
+
+console.log(students);
