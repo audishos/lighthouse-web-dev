55ac399fbe927b63ca3b3d767f4ae77a4594d919
audishos <audisho.sada@gmail.com>
2017-09-08 15:34:22 -0400

    Adds functionality to poppin bottles to output total earned

--- w1d5/poppinBottles/poppinBottles.js
+++ w1d5/poppinBottles/poppinBottles.js
@@ -2,20 +2,26 @@ const bottles = {
   totalBottles: 0,
   currBottles: 0,
   currCaps: 0,
+  totalPurchased: 0,
+  totalFromBottleExchange: 0,
+  totalFromCapExchange: 0,
   exchangeBottles: function() {
     this.totalBottles++; // +1 to the total
     this.currBottles--; // exchange 2 and get 1 back (net -1)
     this.currCaps++; // also add a cap
+    this.totalFromBottleExchange++; // +1 to from bottle exchange count
   },
   exchangeCaps: function() {
     this.totalBottles++; // +1 to the total
     this.currCaps -= 3; // exchange 4 and get 1 back (net -3)
     this.currBottles++; // also add a bottle
+    this.totalFromCapExchange++; // +1 to from cap exchange count
   },
   poppinBottles: function(investment) {
-    this.currBottles = Math.floor(investment / 2);
-    this.currCaps = this.currBottles;
-    this.totalBottles = this.currBottles;
+    this.totalPurchased = Math.floor(investment / 2); // round down (can only have whole bottles)
+    this.currBottles = this.totalPurchased;
+    this.currCaps = this.totalPurchased;
+    this.totalBottles = this.totalPurchased;
 
     // continue exchanging as long as there are
     // at least 2 bottles or at least 4 caps
@@ -27,8 +33,12 @@ const bottles = {
         this.exchangeCaps();
       }
     }
-    return this.totalBottles;
+    console.log("TOTAL BOTTLES: " + this.totalBottles);
+    console.log("TOTAL EARNED: ");
+    console.log(" BOTTLES: " + this.totalFromBottleExchange);
+    console.log(" CAPS: " + this.totalFromCapExchange);
   }
 }
 
-console.log("The total bottles received will be: " + bottles.poppinBottles(Number(process.argv[2])));
+
+bottles.poppinBottles(Number(process.argv[2]));
