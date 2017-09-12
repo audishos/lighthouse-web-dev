d4929b5515e40b1a90c61b9d6849ca52e4a791a8
audishos <audisho.sada@gmail.com>
2017-09-11 20:07:58 -0400

    Comments out algorithmic function in stockMarket.js (didn't finish)

--- w1d5/stockMarket/stockMarket.js
+++ w1d5/stockMarket/stockMarket.js
@@ -1,19 +1,41 @@
-const maxProfit = function(values) {
-  const calculateMaxProfit = function(values, counter) {
-    if (values[counter + 1]) {
-      console.log(values[counter], values[counter +  1]);
-      var curResult = values[counter] - values[counter + 1];
-      var nextResult = calculateMaxProfit(values, counter + 1);
+const maxProfitBruteForce = function(values) {
+  let maxProfit = 0;
+  let left = 0;
+  let right = 1;
 
-      counter++;
-
-      return curResult > nextResult ? curResult : nextResult;
+  while (left < values.length - 1) {
+    let currProfit = values[right] - values[left];
+    if (currProfit > maxProfit) {
+      maxProfit = currProfit;
     }
-
+    if (values[right + 1]) {
+      right++;
+    }
+    else {
+      left++;
+      right = left + 1;
+    }
   }
-  return calculateMaxProfit(values, 0);
-};
+  return maxProfit;
+}
 
-console.log(maxProfit([45, 24, 35, 31, 40, 38, 11]));
+// const maxProfitAlgorithm = function(values) {
+//   // console.log ("indexes", values.indexOf(Math.max.apply(null, values)), values.indexOf(Math.min.apply(null, values)))
+//   // console.log ("values:", Math.max.apply(null, values), Math.min.apply(null, values))
+//   if (values.indexOf(Math.max.apply(null, values)) < values.indexOf(Math.min.apply(null, values))){
+//     return maxProfitAlgorithm(values.slice(values.indexOf(Math.max.apply(null, values)) + 1));
+//   }
+//   else {
+//     // console.log(values);
+//     // console.log(Math.max.apply(null, values), Math.min.apply(null, values));
+//     return Math.max.apply(null, values) - Math.min.apply(null, values);
+//   }
 
-module.exports = maxProfit;
+// }
+
+console.log(maxProfitBruteForce([45, 24, 35, 31, 40, 38, 11]));
+// console.log(maxProfitAlgorithm([45, 24, 35, 31, 40, 38, 11]));
+
+// 45, 29, 94, 83, 58, 13, 90, 12, 56
+
+module.exports = maxProfitBruteForce;
